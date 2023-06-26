d861f85a305167d75a3518ba52ca84807255f154
fadyfaddoul <fady_faddoul@hotmail.com>
2023-06-26 15:06:48 +0300

    adding CommandLineRunner

--- sakila-sprintboot-webapi/src/main/java/org/fadyfadd/sakilaspringbootwebapi/SakilaSprintbootWebapiApplication.java
+++ sakila-sprintboot-webapi/src/main/java/org/fadyfadd/sakilaspringbootwebapi/SakilaSprintbootWebapiApplication.java
@@ -1,5 +1,7 @@
 package org.fadyfadd.sakilaspringbootwebapi;
 
+import org.slf4j.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,10 +9,19 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("org.fadyfadd.sakilaspringbootwebapi")													 	
-public class SakilaSprintbootWebapiApplication {
+public class SakilaSprintbootWebapiApplication implements CommandLineRunner {
 
+	private static Logger logger = LoggerFactory
+		      .getLogger(SakilaSprintbootWebapiApplication.class);
+	
 	public static void main(String[] args) {	 
 		SpringApplication.run(SakilaSprintbootWebapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Starting SakilaSprintbootWebapiApplication");
+		
 	}
 
 }
